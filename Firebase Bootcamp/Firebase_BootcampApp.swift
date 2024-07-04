4a059abdaf3e60e1102fc3f25f9f16da5c50eebe
Pavlo Bezkorovainyi <beskor1993@icloud.com>
2024-07-04 21:03:30 +0300

    firebase set up

--- Firebase Bootcamp/Firebase_BootcampApp.swift
+++ Firebase Bootcamp/Firebase_BootcampApp.swift
@@ -6,12 +6,26 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
-struct Firebase_BootcampApp: App {
+struct YourApp: App {
+  @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
   var body: some Scene {
     WindowGroup {
-      ContentView()
+      NavigationView {
+        ContentView()
+      }
     }
+  }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    print("Firebase configured")
+    return true
   }
 }
