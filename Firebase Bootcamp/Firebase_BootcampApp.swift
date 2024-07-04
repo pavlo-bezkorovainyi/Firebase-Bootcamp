2f1df48d949b45585393a92a5549562c294b49d5
Pavlo Bezkorovainyi <86742948+pavlo-bezkorovainyi@users.noreply.github.com>
2024-07-04 20:37:41 +0300

    Add files via upload

---
+++ Firebase Bootcamp/Firebase_BootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  Firebase_BootcampApp.swift
+//  Firebase Bootcamp
+//
+//  Created by Pavlo Bezkorovainyi on 04.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct Firebase_BootcampApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
